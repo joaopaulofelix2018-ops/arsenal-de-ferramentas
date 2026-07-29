fb394d8fc09d53a989d95d3e4b9a3258eb323fe0
joaopaulofelix2018-ops <joaopaulofelix2018@gmail.com>
2026-07-29 13:03:19 -0300

    Atualizar o constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_NAME = "Arsenal de Ferramentas";
 export const SITE_DESCRIPTION = "As melhores promoções de ferramentas todos os dias, direto no seu WhatsApp.";
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://arsenal-de-ferramentas.vercel.app";
-export const WHATSAPP_LINK = "https://chat.whatsapp.com/HGMbYdIt6fMLoP6Mzts7gb?s=cl&p=i&ilr=4&amv=0";
+export const WHATSAPP_LINK = "https://chat.whatsapp.com/LKxb1OHO39C8SrJjVhw5A1?s=cl&p=i&ilr=4&amv=0";
 export const OFFICIAL_LOGO = "https://i.postimg.cc/XqFqh7PN/1BF6E9FF-9B1D-4557-83DB-9EC76D70048C.png";
